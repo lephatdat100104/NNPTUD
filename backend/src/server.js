3b056d7aa8c34a830492379651a42a0d1ca78f65
Le Phat Dat <chuadat12@gmail.com>
2026-04-07 16:31:53 +0700

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import authRoute from "./routes/authRoute.js";
 import userRoute from "./routes/userRoute.js";
 import cookieParser from "cookie-parser";
 import { protectedRoute } from "./middlewares/authMiddleware.js";
+import cors from "cors";
 
 
 
@@ -16,6 +17,7 @@ const PORT = process.env.PORT || 5001;
 // middlewares
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 
 
